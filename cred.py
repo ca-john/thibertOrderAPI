7eda929146d8f0156d3978bb989c2f7b509fd597
Momin Naseem <mominn3935@gmail.com>
2023-03-19 16:54:12 -0400

    Update gitignore

--- cred.py
+++
@@ -1,7 +0,0 @@
-"""Module for holding credentials for the project."""
-XLSX_FILE: str = 'data.xlsx'
-
-# Thibert credentials
-TAPI_KEY: str = 'D399DF17-1991-49FF-8457-F160E1C5319F'
-JSON_URL: str = 'https://tapidev.azurewebsites.net/swagger/V1/swagger.json'
-THIBERT_BASE_URL: str = 'https://tapidev.azurewebsites.net/swagger/index.html'
